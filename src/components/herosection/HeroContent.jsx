26b60f17384d61f3f6dc0318646096865ad2d40f
newajDev <newaj.design@gmail.com>
2025-07-23 07:18:49 +0600

    bug fixed for banner image

--- src/components/herosection/HeroContent.jsx
+++ src/components/herosection/HeroContent.jsx
@@ -1,11 +1,12 @@
 import HeroTitle from './HeroTitle';
 import HeroDes from './HeroDes';
 import PremaryBtn from '../PremaryBtn';
+import heroBanner from '../../../public/HeroBanner.png';
 
 
 const HeroContent = () => {
     return (
-        <div className='bg-hero-image w-[1280px] bg-no-repeat px-48 py-32' >
+        <div style={{ backgroundImage: `url("${heroBanner}")` }} className='bg-hero-image w-[1280px] bg-no-repeat px-48 py-32' >
             <HeroTitle Title={'Discover an exceptional cooking class tailored for you!'} Style={''}></HeroTitle>
             <HeroDes Description={'Learn and Master Basic Programming, Data Structures, Algorithm, OOP, Database and solve 500+ coding problems to become an exceptionally well world-class Programmer.'}></HeroDes>
             <div className='flex gap-6 justify-center'>
